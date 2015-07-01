0ddc9feb21a74847b5078d484edf6ea665064042
张云来 <cloudcome@163.com>
2015-07-01 19:47:48 +0800

    howdo@2.0.0

--- howdo.js
+++ howdo.js
@@ -10,134 +10,134 @@
  */
 
 
-    'use strict';
+'use strict';
 
-    var _global = typeof window !== 'undefined' ? window : global;
-    var slice = Array.prototype.slice;
-    var noop = function () {
-        // ignore
-    };
-    /**
-     * 判断是否为函数
-     * @param obj
-     * @returns {boolean}
-     */
-    var isFunction = function (obj) {
-        return typeof obj === 'function';
-    };
+var _global = typeof window !== 'undefined' ? window : global;
+var slice = Array.prototype.slice;
+var noop = function () {
+    // ignore
+};
+/**
+ * 判断是否为函数
+ * @param obj
+ * @returns {boolean}
+ */
+var isFunction = function (obj) {
+    return typeof obj === 'function';
+};
 
 
-    /**
-     * 遍历
-     * @param object
-     * @param callback
-     */
-    var each = function (object, callback) {
-        var i;
-        var j;
+/**
+ * 遍历
+ * @param object
+ * @param callback
+ */
+var each = function (object, callback) {
+    var i;
+    var j;
 
-        if (object && object.constructor === Array) {
-            for (i = 0, j = object.length; i < j; i++) {
+    if (object && object.constructor === Array) {
+        for (i = 0, j = object.length; i < j; i++) {
+            callback(i, object[i]);
+        }
+    } else if (typeof object === "object") {
+        for (i in object) {
+            if (object.hasOwnProperty && object.hasOwnProperty(i)) {
+                callback(i, object[i]);
+            } else {
                 callback(i, object[i]);
             }
-        } else if (typeof object === "object") {
-            for (i in object) {
-                if (object.hasOwnProperty && object.hasOwnProperty(i)) {
-                    callback(i, object[i]);
-                } else {
-                    callback(i, object[i]);
-                }
-            }
         }
-    };
-
-    module.exports = {
-        task: function () {
-            if (this.constructor === Howdo) {
-                return this;
-            }
-
-            var args = slice.call(arguments);
-            var howdo = new Howdo();
-
-            return howdo.task.apply(howdo, args);
-        },
-        each: function () {
-            if (this.constructor === Howdo) {
-                return this;
-            }
-
-            var args = slice.call(arguments);
-            var howdo = new Howdo();
-
-            return howdo.each.apply(howdo, args);
-        }
-    };
-
-
-    //////////////////////////////////////////////////////////////////////
-    /////////////////////////[ constructor ]//////////////////////////////
-    //////////////////////////////////////////////////////////////////////
-
-    // 构造函数
-    function Howdo() {
-        var the = this;
-
-        // 任务队列
-        the.tasks = [];
-        // 是否已经开始执行任务了
-        the.hasStart = false;
-        // 标识任务序号
-        the.index = 0;
-        the._tryCallbacks = [];
-        the._catchCallbacks = [];
-        the._allCallback = null;
     }
+};
 
-    Howdo.prototype = {
-        /**
-         * 单次分配任务
-         * @param {Function} fn 任务函数
-         * @return Howdo
-         * @chainable
-         * @example
-         * // next约定为串行执行汇报，后面接follow
-         * // 建议next只返回一个结果
-         * // err对象必须是Error的实例
-         * howdo.task(function(next){
+module.exports = {
+    task: function () {
+        if (this.constructor === Howdo) {
+            return this;
+        }
+
+        var args = slice.call(arguments);
+        var howdo = new Howdo();
+
+        return howdo.task.apply(howdo, args);
+    },
+    each: function () {
+        if (this.constructor === Howdo) {
+            return this;
+        }
+
+        var args = slice.call(arguments);
+        var howdo = new Howdo();
+
+        return howdo.each.apply(howdo, args);
+    }
+};
+
+
+//////////////////////////////////////////////////////////////////////
+/////////////////////////[ constructor ]//////////////////////////////
+//////////////////////////////////////////////////////////////////////
+
+// 构造函数
+function Howdo() {
+    var the = this;
+
+    // 任务队列
+    the.tasks = [];
+    // 是否已经开始执行任务了
+    the.hasStart = false;
+    // 标识任务序号
+    the.index = 0;
+    the._tryCallbacks = [];
+    the._catchCallbacks = [];
+    the._allCallback = null;
+}
+
+Howdo.prototype = {
+    /**
+     * 单次分配任务
+     * @param {Function} fn 任务函数
+     * @return Howdo
+     * @chainable
+     * @example
+     * // next约定为串行执行汇报，后面接follow
+     * // 建议next只返回一个结果
+     * // err对象必须是Error的实例
+     * howdo.task(function(next){
          *     next(new Error('错误'), 1);
          * });
-         *
-         * // done约定为并行执行汇报，后面接together
-         * // 建议done只返回一个结果
-         * // err对象必须是Error的实例
-         * howdo.task(function(){
+     *
+     * // done约定为并行执行汇报，后面接together
+     * // 建议done只返回一个结果
+     * // err对象必须是Error的实例
+     * howdo.task(function(){
          *     done(new Error('错误'), 1);
          * });
-         */
-        task: function (fn) {
-            var the = this;
+     */
+    task: function (fn) {
+        var the = this;
 
-            if (!isFunction(fn)) {
-                throw new Error('howdo `task` must be a function');
-            }
+        if (!isFunction(fn)) {
+            throw new Error('howdo `task` must be a function');
+        }
 
-            fn.index = the.index++;
-            the.tasks.push(fn);
+        fn.index = the.index++;
+        the.tasks.push(fn);
 
-            return the;
-        },
+        return the;
+    },
 
 
-        /**
-         * 循环分配任务
-         * @param  {Object}   object   对象或者数组
-         * @param  {Function} callback 回调
-         * @return Howdo
-         * @example
-         * // follow
-         * // err对象必须是Error的实例
-         * howdo.each([1, 2, 3], function(key, val, next, lastData){
+    /**
+     * 循环分配任务
+     * @param  {Object}   object   对象或者数组
+     * @param  {Function} callback 回调
+     * @return Howdo
+     * @example
+     * // follow
+     * // err对象必须是Error的实例
+     * howdo.each([1, 2, 3], function(key, val, next, lastData){
          *     // lastData 第1次为 undefined
          *     // lastData 第2次为 1
          *     // lastData 第3次为 2
@@ -146,10 +146,10 @@
          *     // err = null
          *     // data = 3
          * });
-         *
-         * // together
-         * // err对象必须是Error的实例
-         * howdo.each([1, 2, 3], function(key, val, done){
+     *
+     * // together
+     * // err对象必须是Error的实例
+     * howdo.each([1, 2, 3], function(key, val, done){
          *     done(null, val);
          * }).together(function(err, data1, data2, dat3){
          *     // err = null
@@ -157,113 +157,113 @@
          *     // data2 = 2
          *     // data3 = 3
          * });
-         */
-        each: function (object, callback) {
-            var howdo = this;
+     */
+    each: function (object, callback) {
+        var howdo = this;
 
-            each(object, task);
+        each(object, task);
 
-            function task(key, val) {
-                howdo = howdo.task(function () {
-                    var args = [key, val];
-                    args = args.concat(slice.call(arguments));
-                    callback.apply(val, args);
-                });
-            }
+        function task(key, val) {
+            howdo = howdo.task(function () {
+                var args = [key, val];
+                args = args.concat(slice.call(arguments));
+                callback.apply(val, args);
+            });
+        }
 
-            return howdo;
-        },
+        return howdo;
+    },
 
 
-        /**
-         * 跟着做，任务串行执行
-         * 链式结束
-         * @example
-         * howdo
-         * .task(function(next){
+    /**
+     * 跟着做，任务串行执行
+     * 链式结束
+     * @example
+     * howdo
+     * .task(function(next){
          *     next(null, 1);
          * })
-         * .task(function(next, data){
+     * .task(function(next, data){
          *     // data = 1
          *     next(null, 2, 3);
          * })
-         * .task(function(next, data1, data2){
+     * .task(function(next, data1, data2){
          *     // data1 = 2
          *     // data2 = 3
          *     next(null, 4, 5, 6);
          * })
-         * .follow(function(err, data1, data2, data3){
+     * .follow(function(err, data1, data2, data3){
          *     // err = null
          *     // data1 = 1
          *     // data2 = 2
          *     // data3 = 3
          * });
-         */
-        follow: function (callback) {
-            var the = this;
+     */
+    follow: function (callback) {
+        var the = this;
 
-            if (the.hasStart) {
-                return the;
-            }
-
-            if (!isFunction(callback)) {
-                callback = noop;
-            }
-
-            the._allCallback = callback;
-            the.hasStart = true;
-
-            var current = 0;
-            var tasks = the.tasks;
-            var count = tasks.length;
-            var args = [];
-
-            if (!count) {
-                the._fixCallback();
-                return the;
-            }
-
-            (function _follow() {
-                var fn = function () {
-                    args = slice.call(arguments);
-
-                    if (args[0]) {
-                        return the._fixCallback(args[0]);
-                    }
-
-                    current++;
-
-                    if (current === count) {
-                        the._fixCallback.apply(the, args);
-                    } else if (current < count) {
-                        args.shift();
-                        _follow();
-                    }
-                };
-
-                args.unshift(fn);
-                tasks[current].apply(_global, args);
-            })();
-
+        if (the.hasStart) {
             return the;
-        },
+        }
+
+        if (!isFunction(callback)) {
+            callback = noop;
+        }
+
+        the._allCallback = callback;
+        the.hasStart = true;
+
+        var current = 0;
+        var tasks = the.tasks;
+        var count = tasks.length;
+        var args = [];
+
+        if (!count) {
+            the._fixCallback();
+            return the;
+        }
+
+        (function _follow() {
+            var fn = function () {
+                args = slice.call(arguments);
+
+                if (args[0]) {
+                    return the._fixCallback(args[0]);
+                }
+
+                current++;
+
+                if (current === count) {
+                    the._fixCallback.apply(the, args);
+                } else if (current < count) {
+                    args.shift();
+                    _follow();
+                }
+            };
+
+            args.unshift(fn);
+            tasks[current].apply(_global, args);
+        })();
+
+        return the;
+    },
 
 
-        /**
-         * 一起做，任务并行执行
-         * 链式结束
-         * @example
-         * howdo
-         * .task(function(done){
+    /**
+     * 一起做，任务并行执行
+     * 链式结束
+     * @example
+     * howdo
+     * .task(function(done){
          *     done(null, 1);
          * })
-         * .task(function(done){
+     * .task(function(done){
          *     done(null, 2, 3);
          * })
-         * .task(function(done){
+     * .task(function(done){
          *     done(null, 4, 5, 6);
          * })
-         * .together(function(err, data1, data2, data3, data4, data5, data6){
+     * .together(function(err, data1, data2, data3, data4, data5, data6){
          *     // err = null
          *     // data1 = 1
          *     // data2 = 2
@@ -272,115 +272,115 @@
          *     // data5 = 5
          *     // data6 = 6
          * });
-         */
-        together: function (callback) {
-            var the = this;
+     */
+    together: function (callback) {
+        var the = this;
 
-            if (the.hasStart) {
-                return;
-            }
+        if (the.hasStart) {
+            return;
+        }
 
-            if (!isFunction(callback)) {
-                callback = noop;
-            }
+        if (!isFunction(callback)) {
+            callback = noop;
+        }
 
-            the._allCallback = callback;
-            the.hasStart = true;
+        the._allCallback = callback;
+        the.hasStart = true;
 
-            var done = 0;
-            var tasks = the.tasks;
-            var count = tasks.length;
-            var taskData = [];
-            var hasCallback = false;
-            var i = 0;
+        var done = 0;
+        var tasks = the.tasks;
+        var count = tasks.length;
+        var taskData = [];
+        var hasCallback = false;
+        var i = 0;
 
-            if (!count) {
-                the._fixCallback();
-                return the;
-            }
+        if (!count) {
+            the._fixCallback();
+            return the;
+        }
 
-            for (; i < count; i++) {
-                _doTask(i, tasks[i]);
-            }
+        for (; i < count; i++) {
+            _doTask(i, tasks[i]);
+        }
 
-            function _doTask(index, task) {
-                var fn = function () {
-                    if (hasCallback) {
-                        return;
+        function _doTask(index, task) {
+            var fn = function () {
+                if (hasCallback) {
+                    return;
+                }
+
+                var args = slice.call(arguments);
+                var ret = [];
+                var i = 0;
+
+                if (args[0]) {
+                    hasCallback = true;
+                    return the._fixCallback(args[0]);
+                }
+
+                taskData[index] = args.slice(1);
+                done++;
+
+                if (done === count) {
+                    for (; i < taskData.length; i++) {
+                        ret = ret.concat(taskData[i]);
                     }
 
-                    var args = slice.call(arguments);
-                    var ret = [];
-                    var i = 0;
+                    ret.unshift(null);
+                    the._fixCallback.apply(the, ret);
+                }
+            };
 
-                    if (args[0]) {
-                        hasCallback = true;
-                        return the._fixCallback(args[0]);
-                    }
+            task(fn);
+        }
 
-                    taskData[index] = args.slice(1);
-                    done++;
+        return the;
+    },
+    /**
+     * 正常回调
+     * @param callback
+     */
+    try: function (callback) {
+        var the = this;
 
-                    if (done === count) {
-                        for (; i < taskData.length; i++) {
-                            ret = ret.concat(taskData[i]);
-                        }
+        if (isFunction(callback)) {
+            the._tryCallbacks.push(callback);
+        }
 
-                        ret.unshift(null);
-                        the._fixCallback.apply(the, ret);
-                    }
-                };
+        return the;
+    },
+    /**
+     * 异常回调
+     * @param callback
+     */
+    catch: function (callback) {
+        var the = this;
 
-                task(fn);
-            }
+        if (isFunction(callback)) {
+            the._catchCallbacks.push(callback);
+        }
 
-            return the;
-        },
-        /**
-         * 正常回调
-         * @param callback
-         */
-        try: function (callback) {
-            var the = this;
+        return the;
+    },
+    /**
+     * 修正回调
+     * @param err
+     * @private
+     */
+    _fixCallback: function (err/*arguments*/) {
+        var the = this;
+        var args = slice.call(arguments, 1);
 
-            if (isFunction(callback)) {
-                the._tryCallbacks.push(callback);
-            }
+        the._allCallback.apply(_global, arguments);
 
-            return the;
-        },
-        /**
-         * 异常回调
-         * @param callback
-         */
-        catch: function (callback) {
-            var the = this;
-
-            if (isFunction(callback)) {
-                the._catchCallbacks.push(callback);
-            }
-
-            return the;
-        },
-        /**
-         * 修正回调
-         * @param err
-         * @private
-         */
-        _fixCallback: function (err/*arguments*/) {
-            var the = this;
-            var args = slice.call(arguments, 1);
-
-            the._allCallback.apply(_global, arguments);
-
-            if (err) {
-                return each(the._catchCallbacks, function (i, callback) {
-                    callback.call(_global, err);
-                });
-            }
-
-            each(the._tryCallbacks, function (i, callback) {
-                callback.apply(_global, args);
+        if (err) {
+            return each(the._catchCallbacks, function (i, callback) {
+                callback.call(_global, err);
             });
         }
-    };
+
+        each(the._tryCallbacks, function (i, callback) {
+            callback.apply(_global, args);
+        });
+    }
+};
